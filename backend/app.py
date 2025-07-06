6b37092169efff17bfb7f54d19f3d8d4f6fcde7f
Vasudev Kishor <vasudevkishor@gmail.com>
2025-07-06 20:00:18 +0530

    restart

--- backend/app.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    # 👇 Explicitly set host and port for clarity
-    app.run(debug=True, host='127.0.0.1', port=5000)
